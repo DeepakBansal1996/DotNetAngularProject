2a647bb7d3fc9b334dbe94a66063977de3fe77eb
deepak9634349532 <deepak.bansal@cygrp.com>
2019-12-20 12:56:33 +0530

    first commit with backend initial setup

---
+++ BackEnd/DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        private DbSet<Value> values;
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get => values; set => values = value; }
+
+    }
+}
